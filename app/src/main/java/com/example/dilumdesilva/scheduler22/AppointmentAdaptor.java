a4f983584347316cb24aa4b30d228791fa52a16c
Dilum De Silva <dilumdesilva.info@gmail.com>
2018-05-02 01:44:47 +0530

    Implemented the search functionality

--- app/src/main/java/com/example/dilumdesilva/scheduler22/AppointmentAdaptor.java
+++ app/src/main/java/com/example/dilumdesilva/scheduler22/AppointmentAdaptor.java
@@ -2,16 +2,45 @@ package com.example.dilumdesilva.scheduler22;
 
 import android.content.Context;
 import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
 import android.widget.ArrayAdapter;
+import android.widget.RelativeLayout;
 import android.widget.TextView;
 
 import com.example.dilumdesilva.scheduler22.Appointment;
 
+import java.util.List;
+
 public class AppointmentAdaptor extends ArrayAdapter<Appointment> {
 
     TextView titleTV , detailsTV;
-    
-    public AppointmentAdaptor(@NonNull Context context, int resource) {
-        super(context, resource);
+
+    public AppointmentAdaptor(Context context, int textViewResourceId, List<Appointment> appointments){
+        super(context, textViewResourceId, appointments);
+    }
+
+    @Override
+    public View getView(int position, View convertView,ViewGroup parent) {
+
+        RelativeLayout row = (RelativeLayout)convertView;
+        if(null == row){
+            //No recycled View, we have to inflate one.
+            LayoutInflater inflater = (LayoutInflater)parent.getContext().getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+            row = (RelativeLayout)inflater.inflate(R.layout.list_row_appointment, null);
+        }
+
+        //initializing the two text views
+        titleTV = (TextView)row.findViewById(R.id.titleTextView);
+        detailsTV = (TextView)row.findViewById(R.id.detailsTextView);
+
+
+        //Set the resulting synonym category and synonyms in the TextViews
+        titleTV.setText(getItem(position).getTitle());
+        detailsTV.setText(getItem(position).getDetails());
+
+        return row;
     }
 }
--- app/src/main/java/com/example/dilumdesilva/scheduler22/SearchAppoinmentScreen.java
+++ app/src/main/java/com/example/dilumdesilva/scheduler22/SearchAppoinmentScreen.java
@@ -1,13 +1,162 @@
 package com.example.dilumdesilva.scheduler22;
 
+import android.content.Context;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.Gravity;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.view.inputmethod.InputMethodManager;
+import android.widget.AdapterView;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ListView;
+import android.widget.PopupWindow;
+import android.widget.TextView;
+import android.widget.Toast;
 
-public class SearchAppoinmentScreen extends AppCompatActivity {
+import com.example.dilumdesilva.scheduler22.DB.SQLiteHandler;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class SearchAppoinmentScreen extends AppCompatActivity implements View.OnClickListener {
+
+    private Button searchBtn;
+    private EditText searchET;
+
+    SQLiteHandler myDBHandler;
+
+    //list view stuff
+    AppointmentAdaptor appointmentAdaptor;
+    ListView listView;
+
+    //lists to store all the resulting appointments
+    List<Appointment> listArr;
+    //list to store matching appointments
+    List<Appointment> listMatches;
+
+    //variable to store the value input from the textbox
+    String searchKeywords;
+
+    //search popup stuff
+    private PopupWindow popupWindow;
+    TextView titleTV,timeTV,detailsTV;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_search_appoinment_screen);
+
+
+        //initialising the button and edit text
+        searchBtn = (Button) findViewById(R.id.confirmButton);
+        searchBtn.setOnClickListener(this);
+        searchET = (EditText) findViewById(R.id.searchEditText);
+
+        //creates an instance of the MyDBHandler
+        myDBHandler = new SQLiteHandler(this, null, null, 1);
+
+        //call the displayappointment method and store all the appointments in a list
+        listArr = myDBHandler.displayAppointments();
+
+        //initialize the list view
+        listView = (ListView) findViewById(R.id.searchList);
+        //adding a list item click listener
+        listView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+
+                displayClickedSearch(appointmentAdaptor.getItem(position) , view);
+
+            }
+        });
+    }
+
+    @Override
+    public void onClick(View v) {
+        //Hides the virtual keyboard when the buttons are clicked
+        InputMethodManager inputManager = (InputMethodManager)
+                getSystemService(Context.INPUT_METHOD_SERVICE);
+        inputManager.hideSoftInputFromWindow(getCurrentFocus().getWindowToken(),
+                InputMethodManager.HIDE_NOT_ALWAYS);
+
+        switch (v.getId()){
+            case R.id.confirmButton : {
+
+                try {
+                    if (searchET.getText().toString().equals("") || searchET.getText().toString().equals(null)) {
+
+                        searchET.setError("Please input a Keyword");
+
+                    } else {
+                        //initialize a new list of apppointments
+                        listMatches = new ArrayList<>();
+
+                        //assign the edit text value to the searchKeywords variable
+                        searchKeywords = searchET.getText().toString();
+
+
+                        //see if the arraylist objectcts contain any of the keywords
+                        for (Appointment appointment : listArr) {
+
+                            if (appointment.getTitle().contains(searchKeywords)) {
+
+                                listMatches.add(appointment);
+                            }
+
+                        }
+
+
+                        appointmentAdaptor = new AppointmentAdaptor(getBaseContext(), -1, listMatches);
+                        listView.setAdapter(appointmentAdaptor);
+
+                        if(listMatches.size() == 0){
+
+                            Toast.makeText(getBaseContext(),"Couldn't find any matches", Toast.LENGTH_SHORT).show();
+
+                        }
+                    }
+                }catch (Exception e){
+
+                    Toast.makeText(getBaseContext(),"Couldn't find any matches", Toast.LENGTH_SHORT).show();
+
+                }
+                searchET.setText("");
+                break;
+            }
+        }
+    }
+
+    public void displayClickedSearch(Appointment appointment , View v){
+
+        Toast.makeText(getBaseContext(),appointment.getTitle(), Toast.LENGTH_SHORT).show();
+
+
+        //get an instance of layoutinflater
+        LayoutInflater inflater = (LayoutInflater) SearchAppoinmentScreen.this
+                .getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+        //initiate the view
+        final View layout = inflater.inflate(R.layout.search_popup,
+                (ViewGroup) findViewById(R.id.searchPoppup_screen));
+
+        //initialising the textviews in search popup
+        titleTV = (TextView) layout.findViewById(R.id.searchedTitle) ;
+        timeTV = (TextView) layout.findViewById(R.id.searchedTime) ;
+        detailsTV = (TextView) layout.findViewById(R.id.searchedDetails) ;
+
+        //initialize a size for the popup
+        popupWindow = new PopupWindow(layout, 1200, 900 ,  true);
+        // display the popup in the center
+        popupWindow.showAtLocation(v, Gravity.CENTER, 0, 0);
+
+        //setting the textviews
+        titleTV.setText(appointment.getTitle());
+        timeTV.setText(appointment.getTime());
+        detailsTV.setText(appointment.getDetails());
+
+
+
     }
 }
